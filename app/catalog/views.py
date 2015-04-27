ab8179a026ae86d6a4ffa2e7be5c532458969865
Vladimir Luss <stainful@gmail.com>
2015-04-27 06:57:07 +0300

    select related types and categories

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -97,7 +97,7 @@ class CatalogView(View):
         if article_key:
             filters['article__icontains'] = article_key
 
-        query_set = Item.objects.filter(is_deleted=False, **filters).prefetch_related("itemsizes_set").distinct()
+        query_set = Item.objects.select_related('type', 'category').filter(is_deleted=False, **filters).prefetch_related("itemsizes_set").distinct()
         if ordering:
             query_set = query_set.order_by(ordering)
 
